739e8ee89e2adc61321c12483dc934d7887405c1
Felix Gnass <fgnass@gmail.com>
2014-02-28 23:14:08 +0100

    update tests

--- test/css.js
+++ test/css.js
@@ -4,20 +4,28 @@ var should = require('should')
 
 describe('all', function() {
 
-  it('should generate CSS', function() {
-    css('button.tl').should.equal('.button{border:none;border-radius:4px;background:#333;color:#fff;}.button.primary{color:blue;}')
-    css('box.tl').should.equal('.button{border:none;border-radius:4px;background:#333;color:#fff;}.button.primary{color:blue;}.box{background:silver;}.box_h1{font-size:2em;}')
-    css('animation.tl').should.equal('@keyframes animation-eye{0%{margin-left:-20%;}to{margin-left:100%;}}.animation{background:black;height:25px;width:200px;overflow:hidden;}.animation_eye{height:100%;width:20%;background:red;animation:animation-eye 4s linear 0s infinite alternate;}')
+  it('should generate CSS', function(done) {
+    css('button.tl', '.button{border:none;border-radius:4px;background:#333;color:#fff;}.button.primary{color:blue;}', done)
+  })
+
+  it('should include dependencies', function(done) {
+    css('box.tl', '.button{border:none;border-radius:4px;background:#333;color:#fff;}.button.primary{color:blue;}.box{background:silver;}.box_h1{font-size:2em;}', done)
+  })
+
+  it('should support inline animations', function(done) {
+    css('animation.tl', '@keyframes animation-eye{0%{margin-left:-20%;}to{margin-left:100%;}}.animation{background:black;height:25px;width:200px;overflow:hidden;}.animation_eye{height:100%;width:20%;background:red;animation:animation-eye 4s linear 0s infinite alternate;}', done)
   })
 
 })
 
-function css(file) {
+function css(file, expected, done) {
   var root = path.join(__dirname, '/fixture')
   var tl = teal({ root: root })
   tl.css.sourcemap = false
   tl.css.normalize = false
   tl.css.autoprefix = false
-  tl.process([ path.join(root, file) ])
-  return tl.resources.get('/main.css').data
+  tl.process([ path.join(root, file) ]).then(function() {
+    tl.resources.get('/main.css').data.should.equal(expected)
+    done(null)
+  }, done)
 }

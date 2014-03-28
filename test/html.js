4135ddd2fc295e2a94bc5d7efe0d0453760dfc77
Felix Gnass <fgnass@gmail.com>
2014-03-28 17:59:21 +0100

    refactor html tests

--- test/html.js
+++ test/html.js
@@ -1,29 +1,28 @@
-var should = require('should')
+var fs = require('fs')
+  , should = require('should')
   , path = require('path')
   , teal = require('../lib')
 
 describe('all', function() {
 
-  it('should generate HTML', function() {
-    var root = path.join(__dirname, 'fixture', 'simple')
-    var button = path.join(root, 'button.tl')
-    var box = path.join(root, 'box.tl')
-    var pbutton = path.join(root, 'p-button.tl')
-    var frag = path.join(root, 'fragment.tl')
-    var a = path.join(root, 'a.tl')
-    var ifelse = path.join(root, 'ifelse.tl')
+  var root = path.join(__dirname, 'fixture', 'simple')
+  var tl = teal()
 
-    var tl = teal()
+  var files = fs.readdirSync(root)
+    .filter(function(f) { return path.extname(f) =='.html' })
+    .map(function(f) { return path.join(root, f) })
 
-    tl.process([button, box, pbutton, frag, a, ifelse])
+  function template(f) {
+    return f.replace('.html', '.tl')
+  }
 
-    tl.html.render(button, { content: 'Click Me'}).should.equal('<button class="button">Click Me</button>')
-    tl.html.render(button, { content: 'Click Me', primary: true }).should.equal('<button class="button primary">Click Me</button>')
-    tl.html.render(box, { title: 'Hello', label: 'Click Me' }).should.equal('<div class="box"><h1 class="box_h1">Hello</h1><button class="button">Click Me</button></div>')
-    tl.html.render(pbutton, {}).should.equal('<p class="para p-button"><button class="button">Click</button></p>')
-    tl.html.render(frag, {}).should.equal('<p class="para">Hello</p><p class="para">World</p>')
-    tl.html.render(a, { content: 'Hello', href: '#' }).should.equal('<a class="a" href="#">Hello</a>')
-    tl.html.render(ifelse, { hey: 'Hey!' }).should.equal('<p class="para">Hey!</p>')
+  tl.process(files.map(template))
+
+  files.forEach(function(f) {
+    it(path.basename(f), function() {
+      var html = fs.readFileSync(f, 'utf8').trim()
+      tl.html.render(template(f), {}).should.equal(html)
+    })
   })
 
 })

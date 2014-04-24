9f8af8d29c34bc301cfb14fe4c21013599ac5c2e
Felix Gnass <fgnass@gmail.com>
2014-04-25 01:06:46 +0200

    .

--- test/css.js
+++ test/css.js
@@ -5,11 +5,11 @@ var should = require('should')
 describe('all', function() {
 
   it('should generate CSS', function() {
-    css('button.tl', '.button{border:none;border-radius:4px;background:#333;color:#fff;}.button.primary{color:blue;}')
+    css('a.tl', '.a{color:#000;}')
   })
 
   it('should include dependencies', function() {
-    css('box.tl', '.button{border:none;border-radius:4px;background:#333;color:#fff;}.button.primary{color:blue;}.box{background:silver;}.box_h1{font-size:2em;}')
+    css('box.tl', '.a{color:#000;}.box{background:silver;}.box_h1{font-size:2em;}')
   })
 
   it('should style anonymous inline blocks', function() {
@@ -18,6 +18,14 @@ describe('all', function() {
 
   it('should support inline animations', function() {
     css('animation.tl', '@keyframes animation-eye{0%{margin-left:-20%;}to{margin-left:100%;}}.animation{background:black;height:25px;width:200px;overflow:hidden;}.animation_eye{height:100%;width:20%;background:red;animation:animation-eye 4s linear 0s infinite alternate;}')
+  })
+
+  it('should support parent selectors', function() {
+    css('parent.tl', '.a{color:#000;}.parent:hover > .a{color:blue;}.parent:hover .parent_c{color:yellow;}.parent:hover > b > div{color:pink;}.parent:hover > b{color:green;}')
+  })
+
+  it('should generate state rules', function() {
+    css('button.tl', '.button{border:none;border-radius:4px;background:#333;color:#fff;}.button.primary{color:blue;}.button.primary.disabled{color:#000;}.button.disabled{background:#999;}')
   })
 
 })

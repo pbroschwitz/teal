6e29b5ba47db463aad0f6923d645244b3a992c07
Felix Gnass <fgnass@gmail.com>
2014-09-29 11:59:43 +0200

    .

--- test/util/teal-mock.js
+++ test/util/teal-mock.js
@@ -1,6 +1,8 @@
 module.exports = {
   macros: require('../../lib/macros'),
-
+  css: {
+    functions: require('../../lib/css/functions'),
+  },
   expose: function(path) {
     return { url: 'exposed' }
   }
